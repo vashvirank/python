957fc4e2d96a1487f48a67f784bf6068e564cc00
Vashvi Rank <rankvashvi@gmail.com>
2025-02-06 06:46:08 +0530

    Update array.py

--- numpy/array.py
+++ numpy/array.py
@@ -1,1 +1,28 @@
 import numpy as np
+a = np.array([1, 2, 3, 4])
+print(a)
+#[1 2 3 4]
+type(a)
+# numpy.ndarray
+a.ndim
+# 1
+a.size
+# 4
+arr = np.array([[1, 2, 3], [4, 5, 6]]);
+print(arr)
+# [[1 2 3]
+ # [4 5 6]]
+arr.ndim
+# 2
+arr.size
+# 6
+arr.shape
+# (2, 3)
+arr.dtype
+# dtype('int32')
+ar = np.array([[1, 'a', "world"], [3, 4, 5]])
+ar.dtype
+# dtype('<U11')
+arr
+# array([[1, 2, 3],
+#        [4, 5, 6]])
